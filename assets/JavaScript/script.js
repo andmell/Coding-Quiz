d00a500eeb2699fc12211e0edfe0cf34f7f45847
ahmell <andrewmell10@gmail.com>
2023-07-10 18:14:18 -0500

    added comments to all code to better tool around with it later on

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -107,14 +107,20 @@ function resetState(){
 function selectAnswer(e){
     // We are defining 'selectedBtn' is whatever the user clicked on, which is e.target
     const selectedBtn = e.target;
-    // We are defining the isCorrect variable to refer to the dataset in the quizQuestions array. This variable can be either true or false depending on the 
+    // We are defining the isCorrect variable to refer to the dataset in the quizQuestions array. This variable can be either true or false depending on what is listed in the array.
     const isCorrect = selectedBtn.dataset.correct === "true";
+    // The following function will determine what will happen if the answer is true or false.
     if (isCorrect){
+        // This will add the 'correct' class to the answer, if it's correct. This is important for css styling.
         selectedBtn.classList.add("correct");
+        // if the selected answer is correct, the score will be incremented by 1.
         score++;
+        // if the selected answer is incorrect, the following code will run.
     } else {
+        // applying an incorrect class to the answer, good for css styling.
         selectedBtn.classList.add("incorrect");
     }
+    // The following function will disable answer buttons and display the next button once an answer has been picked. It will also display the correct answer if an inccorect answer was picked.
     Array.from(answerButtons.children).forEach(button => {
         if(button.dataset.correct === "true"){
             button.classList.add("correct");
@@ -124,24 +130,31 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
+// This next block of code will define the showScore function, which will show the score at the end of the quiz. 
 function showScore(){
+    // We will reset the state of the page, which removes all buttons, and hides the next button.
     resetState();
+    // We will manually change the question element HTML to show us our score vs. our possible score.
     questionElement.innerHTML = `You scored ${score} out of ${quizQuestions.length}!`;
+    // The HTML of the nextButton will change to "play again" and will be unhidden.
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
 
-
+// This block of code will define the handleNextButton function
 function handleNextButton(){
+    //We will advance the current question index by 1 each time this function is called.
     currentQuestionIndex++;
+    // This if loop will only run if the current question index is less than the length of our quiz questions array. It will show the repsective question if true.
     if(currentQuestionIndex < quizQuestions.length){
         showQuestion();
+        // if the current question index = the length of the quiz questions, we will show the score instead.
     } else {
         showScore();
     }
 }
 
-
+// This code defines what happens if the user clicks on the next button.
 nextButton.addEventListener("click", ()=>{
     if(currentQuestionIndex < quizQuestions.length){
         handleNextButton();
@@ -160,3 +173,9 @@ startQuiz();
 // The end of the quiz is achieved when all questions have been answered.
 // The end of the quiz is also achieved if the timer reaches 0.
 // Once the quiz ends, the total amount of points is shown compared to the possible amount of points. 
+
+
+// The initial page of the quiz should should have the header that welcomes the user to the quiz, h2 should give the rules, and the next button should say "start quiz". The start quiz function
+// should start the function that runs the timer.
+
+// At the end of the quiz the user should be prompted to 
